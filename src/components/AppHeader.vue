3fef49b46dafa6b2654f88426683abc3a4bc8dbc
Michael <michael.fitzhavey@gmail.com>
2018-07-15 18:23:00 +0100

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="AppHeader">
+	<div class="App__inner App__inner--fullHeight">
+
+		<b-link to="/">
+			<h1 class="AppHeader__title App--center">
+				Rachel Lewis
+			</h1>
+		</b-link>
+
+		<menue
+			class="AppHeader__menue App--center"
+			:auto="true"/>
+
+	</div>
+</div>
+</template>
+
+<script>
+import Menue from './Menue';
+
+export default {
+	components: {
+		Menue
+	}
+};
+
+</script>
+
+<style lang="scss">
+@import '../settings';
+
+.AppHeader{
+	background: $Light-Grey;
+	height: $Header-Height;
+	position: fixed;
+	color: $Dark-Grey;
+	width: 100%;
+	z-index: 10;
+
+	&__title{
+		height: $Header-Height / 2;
+		color: $Dark-Grey;
+		margin: auto;
+		display: block;
+		display: inline-block;
+		left: 0;
+	}
+
+	&__menue{
+		display: inline-block;
+		right: 0;
+		margin-top: 9px;
+	}
+
+}
+
+</style>
